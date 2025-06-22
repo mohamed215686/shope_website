8d71c381d09b7b52696df33f59fdf1b9052ccd3f
Your GitHub Username <your.email@example.com>
2025-06-22 16:34:06 +0100

    SECOND

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -1,7 +1,13 @@
 import React from "react";
 import {MdLocalShipping} from 'react-icons/md'
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiLogIn } from "react-icons/fi";
+import { CiLogout ,CiUser} from "react-icons/ci";
+import { useAuth0 } from "@auth0/auth0-react";
+import {Link} from 'react-router-dom'
 import './nav.css'
 const Nav =()=>{
+    const { loginWithRedirect ,logout,user, isAuthenticated } = useAuth0();
   return(
     <>
     <div className="header">
@@ -13,7 +19,70 @@ const Nav =()=>{
                 <p>Free Shipping </p>
             </div>
         </div>
-        
+        <div className="mid_header">
+          <div className="logo">
+            <img src='images/logo.png' alt='logo'></img>
+          </div>
+          <div className="search_box">
+            <input type='text' value='' placeholder="search"></input>
+            <button><AiOutlineSearch /></button>
+          </div>
+          {
+            isAuthenticated?
+            <div className="user">
+            <div className="icon">
+                <CiLogout />
+            </div>
+            <div className="btn">
+              <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</button>
+            </div>
+          </div>
+          :
+          <div className="user">
+            <div className="icon">
+                <FiLogIn />
+            </div>
+            <div className="btn">
+              <button onClick={() => loginWithRedirect()}>Login</button>
+            </div>
+          </div>
+          }
+        </div>
+        <div className="last_header">
+          <div className="user_profile">
+            {
+              isAuthenticated ?
+              <>
+              <div className="icon">
+                <CiUser />
+              </div>
+              <div className="info">
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+              </div>
+              
+              </>
+              :
+              <>
+              <div className="icon">
+                <CiUser />
+              </div>
+              <div className="info">
+                <p>please Login</p>
+              </div>
+              </>
+            }
+          </div>
+          <div className="nav">
+            <ul>
+              <li><Link to="/" className="link">Home</Link></li>
+              <li><Link to="/shope" className="link">Shope</Link></li>
+              <li><Link to="/collection" className="link">Collection</Link></li>
+              <li><Link to="/about" className="link">About</Link></li>
+              <li><Link to="/contact" className="link">Contact</Link></li>
+            </ul>
+          </div>
+        </div>
     </div>
     </>
   )
